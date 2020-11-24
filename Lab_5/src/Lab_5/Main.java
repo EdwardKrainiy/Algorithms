db107728cbf914252ba6c8245b2c75e1d69b5382
Edvard Krainiy <ekrayniy@inbox.ru>
2020-11-24 15:31:10 +0300

    Added Bipartite and Component task

---
+++ Lab_5/src/Lab_5/Main.java
@@ -0,0 +1,29 @@
+package Lab_5;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Main {
+    public static void main(String[] arr){
+        int[][] matr = {
+                {0,1,0,0,0},
+                {1,0,1,0,0},
+                {0,1,0,1,0},
+                {0,0,1,0,1},
+                {0,0,0,1,0}};
+
+        System.out.println("People task: ");
+        ComponentTask matrix = new Lab_5.ComponentTask(matr);
+        System.out.println(matrix
+                .FindСomponents()
+                .stream()
+                .map(ArrayList::size)
+                .max(Comparator.naturalOrder())
+                .orElse(0)
+        );
+
+        System.out.println("Is bilobed task:");
+        BipartiteTask bipartition = new BipartiteTask(matr);
+        System.out.println(bipartition.IsBilobed(0));
+    }
+}
